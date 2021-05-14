d7e098c3fded7c13317d358e601684b5b941d4cb
9jiya <74593564+9jiya@users.noreply.github.com>
2021-05-14 19:33:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+song = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+function preload(){
+    song = loadSound('m.mp3');
+}
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.position(400,350);
+    video = createCapture(VIDEO);
+    video.hide();
+    x = ml5.poseNet(video,modelLoaded);
+    x.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("Posenet is initialized");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist x is "+rightWristX+"Right wrist y is "+rightWristY);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left Wrist x is "+leftWristX+"left wrist y is "+leftWristY);
+    }
+}
+function draw(){
+    image(video,0,0,600,500);
+    fill('#acfaed');
+    stroke('#010814');
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        x = Number(leftWristY);
+        y = floor(x);
+        z = y/500;
+        song.setVolume(z);
+        document.getElementById("vol_label").innerHTML = "Volume : "+z;
+    }
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX,rightWristY,20);
+        if(rightWristY > 0 && rightWristY <= 100){
+            song.rate(0.5);
+            document.getElementById("speed_label").innerHTML = "Speed = 0.5px";
+        }
+        else if(rightWristY > 100 && rightWristY <= 200){
+            song.rate(1);
+            document.getElementById("speed_label").innerHTML = "Speed = 1px";
+        }
+        else if(rightWristY > 200 && rightWristY <= 300){
+            song.rate(1.5);
+            document.getElementById("speed_label").innerHTML = "Speed = 1.5px";
+        }
+        else if(rightWristY > 300 && rightWristY <= 400){
+            song.rate(2);
+            document.getElementById("speed_label").innerHTML = "Speed = 2px";
+        }
+        else if(rightWristY > 400){
+            song.rate(2.5);
+            document.getElementById("speed_label").innerHTML = "Speed = 2.5px";
+        }
+    }
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
